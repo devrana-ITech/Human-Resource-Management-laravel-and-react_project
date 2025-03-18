fae9740d840fc02e0026e92a8c0c17a4da1bd03b
Ryan <ranahossainnun143.gl@gmail.com>
2025-03-18 12:15:12 +0600

    delete route appjsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ import Sidebar from "./components/Sidebar"
 import HomePage from "./pages/HomePage"
 import Footer from "./components/Footer"
 import Index from "./pages/Index"
-import CreacteProduction from "./pages/production/CreacteProduction"
-import ManageProduction from "./pages/production/ManageProduction"
 import Dashboard from "./pages/Dashboard"
 import SetSalary from "./pages/payroll/SetSalary"
 
@@ -23,8 +21,6 @@ function App() {
             <Route path="/" element={<HomePage/>}> 
               <Route index element={<Index/>}/>
               <Route path="/setsalary" element={<SetSalary/>} />
-              <Route path="/createproduction" element={<CreacteProduction/>} />
-              <Route path="/manageproduction" element={<ManageProduction/>} />
               <Route path="/dashboard" element={<Dashboard/>}/> 
             </Route>
           </Routes>
